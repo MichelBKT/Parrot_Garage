99f951c1525b11fc4c60e31a2ed2751bf17d296d
Michel Banckaert <banckaertm@hotmail.com>
2023-05-25 16:56:14 +0200

    config + home page

--- src/Controller/FooterController.php
+++ src/Controller/FooterController.php
@@ -6,14 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class FooterController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    
+    #[Route('/footer', name: 'app_footer')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('footer/index.html.twig', [
+            'controller_name' => 'FooterController',
         ]);
     }
 }
